1c93824502ed13b39b2fd5a2943a5c51a24fc58e
Galinna Valentina <ferdinandvalentino1@gmail.com>
2025-05-01 16:06:43 +0700

    Reverse Still Not Fixed

--- js/script.js
+++ js/script.js
@@ -4,48 +4,68 @@ function calculateForm() {
     var finalInput = numberInput * 9/5 + 32;
     var unitC = document.getElementById("unit-label-C").textContent;
     var unitF = document.getElementById("unit-label-F").textContent;
+    var oriInput = document.getElementById("input-label").textContent;
+    var oriOutput = document.getElementById("output-label").textContent;
+
 
     if ( conversionInput === "") {
         alert("Please input a number")
         return false;
     }
 
-    setSenderUI(conversionInput, finalInput, unitC, unitF);
+    setSenderUICtoF(conversionInput, finalInput, unitC, unitF, oriInput, oriOutput);
     return false;
 }
 
-function setSenderUI(conversionInput, finalInput, unitC, unitF) {
-    document.getElementById("temp-output").innerHTML = finalInput;
-    document.getElementById("calculate-detail").innerHTML = conversionInput.value + unitC + " * (9/5) + 32 = " + finalInput + unitF;
+function setSenderUICtoF(conversionInput, finalInput, unitC, unitF, oriInput, oriOutput) {
+    document.getElementById("input-label").innerHTML = oriInput;
+    document.getElementById("output-label").innerHTML = oriOutput;
+    document.getElementById("temp-output").value = finalInput;
+    document.getElementById("calculate-detail").value = conversionInput.value + unitC + " * (9/5) + 32 = " + finalInput + unitF;
 }
 
 
 function resetForm() {
     document.getElementById("temp-input").value = "";
-    document.getElementById("temp-output").innerHTML = "";
-    document.getElementById("calculate-detail").innerHTML = "";
+    document.getElementById("temp-output").value = "";
+    document.getElementById("calculate-detail").value = "";
 }
 
+var counter = 0;
 
-function reverseForm() {
+function reverseForm1() {
+    counter++;
     var conversionInput = document.forms["form-input"]["temp-input"];
     var numberInput = parseInt(conversionInput.value);
     var finalInput = numberInput * 9/5 + 32;
     var unitC = document.getElementById("unit-label-C").textContent;
     var unitF = document.getElementById("unit-label-F").textContent;
+    var oriInput = document.getElementById("input-label").textContent;
+    var oriOutput = document.getElementById("output-label").textContent;
 
     if ( conversionInput === "") {
         alert("Please input a number")
         return false;
     }
 
-    setSenderUI(conversionInput, finalInput, unitC, unitF);
+    if (counter % 2 === 1) {
+        console.log("Yes")
+    }
+    if (counter % 2 === 0) {
+        console.log("No")
+    }
+
+    setSenderUIFtoC(conversionInput, finalInput, unitC, unitF, oriInput, oriOutput);
     return false;
 }
 
-function setSenderUI(conversionInput, finalInput, unitC, unitF) {
-    document.getElementById("temp-input").value = finalInput
-    document.getElementById("temp-output").innerHTML = conversionInput.value;
-    document.getElementById("calculate-detail").innerHTML = "(" + finalInput + unitF +  " - 32) * 5/9  = " + conversionInput.value + unitC ;
-}
+
+function setSenderUIFtoC(conversionInput, finalInput, unitC, unitF, oriInput, oriOutput) {
+        document.getElementById("input-label").innerHTML = oriOutput;
+        document.getElementById("output-label").innerHTML = oriInput;
+        document.getElementById("temp-input").value = finalInput;
+        document.getElementById("temp-output").value = (conversionInput.value-32) * 5/9;
+        document.getElementById("calculate-detail").value = "(" + finalInput + unitF +  
+        " - 32) * 5/9  = " + (conversionInput.value-32) * 5/9 + unitC ;
+    }
 
